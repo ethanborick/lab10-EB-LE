5a82e49289b2d3c4c5e52a3efefc9576d1043eae
lukee <LukeEasterling2005@gmail.com>
2025-04-10 10:19:23 -0400

    Added raise errors for division and logarithmic functions

--- calculator.py
+++ calculator.py
@@ -5,7 +5,19 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
+import math
+def add(a, b):
     pass
-
-
+def sub(a, b):
+    pass
+def mul(a, b):
+    pass
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("division by zero")
+    return b / a
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("math domain error")
+    return math.log(b, a)
+def exp(a, b): a^b
